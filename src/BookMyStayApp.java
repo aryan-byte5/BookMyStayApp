8aed6e648a71cfcedb2139cfba4723d6f46aabee
aryan-byte5 <ak9470@srmist.edu.in>
2026-03-18 09:16:18 +0530

    UC1:Application-Entry-&Welcome-Message

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,2 +1,14 @@
 public class BookMyStayApp {
+
+    public static void main(String[] args) {
+
+        String appName = "Book My Stay - Hotel Booking System";
+        String version = "v1.0";
+
+        System.out.println("======================================");
+        System.out.println(" Welcome to " + appName);
+        System.out.println(" Version: " + version);
+        System.out.println("======================================");
+        System.out.println("Thank you for using our application!");
+    }
 }
