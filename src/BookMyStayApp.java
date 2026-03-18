bbb928a964ddfee1ba093104511c2a21db8070f5
aryan-byte5 <ak9470@srmist.edu.in>
2026-03-18 09:24:10 +0530

    UC2:Basic-Room-Types-&-Static-Availability

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,14 +1,72 @@
-public class BookMyStayApp {
+abstract class BookMyStayApp {
+    String type;
+    int beds;
+    double price;
+
+    Room(String type, int beds, double price) {
+        this.type = type;
+        this.beds = beds;
+        this.price = price;
+    }
+
+    abstract void displayDetails();
+}
+
+class SingleRoom extends Room {
+    SingleRoom() {
+        super("Single Room", 1, 2000);
+    }
+
+    void displayDetails() {
+        System.out.println(type + " | Beds: " + beds + " | Price: ₹" + price);
+    }
+}
+
+class DoubleRoom extends Room {
+    DoubleRoom() {
+        super("Double Room", 2, 3500);
+    }
+
+    void displayDetails() {
+        System.out.println(type + " | Beds: " + beds + " | Price: ₹" + price);
+    }
+}
+
+class SuiteRoom extends Room {
+    SuiteRoom() {
+        super("Suite Room", 3, 6000);
+    }
+
+    void displayDetails() {
+        System.out.println(type + " | Beds: " + beds + " | Price: ₹" + price);
+    }
+}
+
+public class UseCase2RoomInitialization {
 
     public static void main(String[] args) {
 
-        String appName = "Book My Stay - Hotel Booking System";
-        String version = "v1.0";
+        String version = "v2.1";
 
-        System.out.println("======================================");
-        System.out.println(" Welcome to " + appName);
-        System.out.println(" Version: " + version);
-        System.out.println("======================================");
-        System.out.println("Thank you for using our application!");
+        Room single = new SingleRoom();
+        Room dbl = new DoubleRoom();
+        Room suite = new SuiteRoom();
+
+        int singleAvailable = 5;
+        int doubleAvailable = 3;
+        int suiteAvailable = 2;
+
+        System.out.println("===== Book My Stay - Hotel Booking System =====");
+        System.out.println("Version: " + version);
+        System.out.println("===============================================");
+
+        single.displayDetails();
+        System.out.println("Available: " + singleAvailable);
+
+        dbl.displayDetails();
+        System.out.println("Available: " + doubleAvailable);
+
+        suite.displayDetails();
+        System.out.println("Available: " + suiteAvailable);
     }
 }
